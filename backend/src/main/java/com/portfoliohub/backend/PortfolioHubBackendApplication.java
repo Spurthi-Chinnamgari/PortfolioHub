10eb74932a80d57fa656b4fa527068ae8913aec2
Spurthi Chinnamgari <chinnamgarispurthi0704@gmail.com>
2026-08-10 15:53:38 +0530

    feat: add initial Spring Boot backend

---
+++ backend/src/main/java/com/portfoliohub/backend/PortfolioHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliohub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioHubBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/portfoliohub/backend/PortfolioHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliohub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
