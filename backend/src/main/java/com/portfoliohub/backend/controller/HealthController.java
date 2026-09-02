76887b2a7c3f1cb83fe5662720f36df16333c8e1
Spurthi Chinnamgari <chinnamgarispurthi0704@gmail.com>
2026-09-03 01:01:15 +0530

    Changed healthz

--- backend/src/main/java/com/portfoliohub/backend/controller/HealthController.java
+++ backend/src/main/java/com/portfoliohub/backend/controller/HealthController.java
@@ -3,8 +3,15 @@ package com.portfoliohub.backend.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 public class HealthController {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("🔥🔥🔥 HEALTH CONTROLLER LOADED 🔥🔥🔥");
+    }
 
     @GetMapping("/healthz")
     public String health() {
